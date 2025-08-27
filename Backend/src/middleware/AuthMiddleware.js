8b1b4152746f34b873919228478cebb58b048999
vatsaltala <vatsaltala806@gmail.com>
2025-08-27 11:04:33 +0530

    Update AuthMiddleware.js

--- Backend/src/middleware/AuthMiddleware.js
+++ Backend/src/middleware/AuthMiddleware.js
@@ -1,35 +1,3 @@
-// const jwt = require('jsonwebtoken');
-// const jwt_secret = "ExpenseTrackerSecretKey";
-
-// const authMiddleware = (req, res, next) => {
-
-//     let token = req.headers.authorization;
-
-//     if (!token) {
-//         return res.status(401).json({ message: 'Token Not Genrate...' });
-//     }
-
-//     if (token.startsWith('Bearer')) {
-//         token = token.split(' ')[1];
-
-//         try {
-//             const varifyToken = jwt.verify(token, jwt_secret)
-//             console.log(varifyToken)
-//             next()
-//         } catch (error) {
-//             return res.status(500).json({ message: 'Token is not valid...' });
-//         }
-
-//     }
-//     else {
-//         return res.status(401).json({ message: 'Token Not Valid...' });
-//     }
-
-// }
-
-// module.exports = { authMiddleware };
-
-//token --> controller -->
 const jwt = require("jsonwebtoken")
 const secret = "secret"
 
